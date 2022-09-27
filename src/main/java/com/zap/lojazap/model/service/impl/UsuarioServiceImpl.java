57344b09aade8557d61d1084d87aadde271a2d7e
Tzappiello <tzappiello@gmail.com>
2022-09-27 19:44:19 -0300

    Treino de Spring aplicação de uma anotation no Usuario

--- src/main/java/com/zap/lojazap/model/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/zap/lojazap/model/service/impl/UsuarioServiceImpl.java
@@ -1,12 +1,22 @@
 package com.zap.lojazap.model.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.zap.lojazap.model.entity.Usuario;
 import com.zap.lojazap.model.repository.UsuarioRepository;
 import com.zap.lojazap.model.service.UsuarioService;
 
+@Service
 public class UsuarioServiceImpl implements UsuarioService {
 	
-	private UsuarioRepository usuarioRepository;
+	@Autowired
+	private UsuarioRepository usuarioRepository;	
+
+	public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+		super();
+		this.usuarioRepository = usuarioRepository;
+	}
 
 	@Override
 	public Usuario autenticar(String nome, String senha) {
