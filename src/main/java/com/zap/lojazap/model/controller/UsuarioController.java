b5c5be6731311705a52bc29bfe55e0624b95b17c
Tzappiello <tzappiello@gmail.com>
2022-09-28 18:34:01 -0300

    Treino de Spring metodo de Criar usuario

--- src/main/java/com/zap/lojazap/model/controller/UsuarioController.java
+++ src/main/java/com/zap/lojazap/model/controller/UsuarioController.java
@@ -1,14 +1,51 @@
 package com.zap.lojazap.model.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zap.lojazap.model.dto.UsuarioDTO;
+import com.zap.lojazap.model.entity.Usuario;
+import com.zap.lojazap.model.exception.RegraDeNegocioException;
+import com.zap.lojazap.model.service.UsuarioService;
+
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
 	
-	@GetMapping
-	public String mostrarDados() {
-		return "Ola estou treinando spring";
+	@PostMapping
+	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+		
+		Usuario usuario = Usuario.builder()
+				.nome(dto.getNome())
+				.email(dto.getEmail())
+				.senha(dto.getSenha()).build();
+		
+		try {
+			Usuario salvarUsuario = usuarioService.salvarUsuario(usuario);
+			return new ResponseEntity(salvarUsuario, HttpStatus.CREATED);
+		} catch (RegraDeNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/zap/lojazap/model/dto/UsuarioDTO.java
@@ -0,0 +1,13 @@
+package com.zap.lojazap.model.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UsuarioDTO {
+
+	private String email;
+	private String nome;
+	private String senha;
+}
