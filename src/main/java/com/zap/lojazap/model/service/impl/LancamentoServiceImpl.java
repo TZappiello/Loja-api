bf6d7556e4c4df7494cd226444fcae848c336357
Tzappiello <tzappiello@gmail.com>
2022-09-29 16:25:43 -0300

    Treino de Spring Criação do metodo de salvar lancamento

--- src/main/java/com/zap/lojazap/model/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/zap/lojazap/model/service/impl/LancamentoServiceImpl.java
@@ -1,5 +1,50 @@
 package com.zap.lojazap.model.service.impl;
 
-public class LancamentoServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zap.lojazap.model.entity.Lancamento;
+import com.zap.lojazap.model.enums.StatusLancamento;
+import com.zap.lojazap.model.repository.LancamentoRepository;
+import com.zap.lojazap.model.service.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+	
+	@Override
+	@Transactional
+	public Lancamento salvar(Lancamento lancamento) {
+		return lancamentoRepository.save(lancamento);
+	}
+
+	@Override
+	public Lancamento atualizar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Lancamento> buscar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
