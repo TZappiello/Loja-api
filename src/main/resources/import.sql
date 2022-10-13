96237e0d04236bdf5bd541083179bdfd4356b4d0
Tzappiello <tzappiello@gmail.com>
2022-10-13 14:26:30 -0300

    Treino de spring implementando arquivo sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into cozinha (nome) values ('Chinesa');
+insert into cozinha (nome) values ('Mexicana');
+insert into cozinha (nome) values ('Tailandesa');
+
+insert into restaurante (nome) values ('Mestre cuca');
+insert into restaurante (nome) values ('Du Chefe');
