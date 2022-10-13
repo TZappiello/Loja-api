9f63cde513d68d27254a0712909049837891221f
Tzappiello <tzappiello@gmail.com>
2022-10-13 14:11:04 -0300

    Treino de spring correção da entidade e add valor do id auto

--- src/main/java/com/zap/lojazap/domaindois/model/Cozinha.java
+++ src/main/java/com/zap/lojazap/domaindois/model/Cozinha.java
@@ -1,18 +1,17 @@
 package com.zap.lojazap.domaindois.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "tab_cozinhas")
 public class Cozinha {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "nom_cozinha", length = 30 )
 	private String nome;
 
 	public Long getId() {
--- src/main/java/com/zap/lojazap/domaindois/model/Restaurante.java
+++ src/main/java/com/zap/lojazap/domaindois/model/Restaurante.java
@@ -4,17 +4,20 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Restaurante {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String nome;
 
-	@Column(name = "taxa_frete", length = 30)
+	@Column(name = "taxa_frete")
 	private BigDecimal taxaFrente;
 
 	public Long getId() {
