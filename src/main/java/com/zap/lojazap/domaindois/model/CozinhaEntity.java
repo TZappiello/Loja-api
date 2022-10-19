808f63a1c0286dc4eda3a241b2cc15176f1ab35f
Tzappiello <tzappiello@gmail.com>
2022-10-19 16:21:59 -0300

    Treino de spring

--- src/main/java/com/zap/lojazap/domaindois/model/CozinhaEntity.java
+++ src/main/java/com/zap/lojazap/domaindois/model/CozinhaEntity.java
@@ -7,10 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRootName;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
