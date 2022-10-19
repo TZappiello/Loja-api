97ba7d8c2f49b0423c99a9c4fc6e9ae97bdef121
Tzappiello <tzappiello@gmail.com>
2022-10-19 15:49:59 -0300

    Treino de spring anotações uteis

--- src/main/java/com/zap/lojazap/domaindois/model/CozinhaEntity.java
+++ src/main/java/com/zap/lojazap/domaindois/model/CozinhaEntity.java
@@ -1,16 +1,22 @@
 package com.zap.lojazap.domaindois.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+//@JsonRootName("gastronomia") muda o nome em XML
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @AllArgsConstructor
@@ -24,6 +30,9 @@ public class CozinhaEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+//	@JsonIgnore  Ignora essa propriedade
+//	@JsonProperty("titulo") muda o nome em JSON e XML
+	@Column(nullable = false)
 	private String nome;
 
 }
