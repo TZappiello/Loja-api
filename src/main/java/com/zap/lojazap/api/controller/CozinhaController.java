af451de0fd5bfa973ed2e87757a4a69d504df270
Tzappiello <tzappiello@gmail.com>
2022-10-20 10:29:52 -0300

    Treino de Spring Implementação de método buscar por id

--- src/main/java/com/zap/lojazap/api/controller/CozinhaController.java
+++ src/main/java/com/zap/lojazap/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.zap.lojazap.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,23 +17,27 @@ import com.zap.lojazap.domaindois.repository.CozinhaRepository;
 @RestController
 @RequestMapping("/cozinhas")
 public class CozinhaController {
-	
+
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
-	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	public List<CozinhaEntity> listar(){
+
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public List<CozinhaEntity> listar() {
 		return cozinhaRepository.todas();
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<CozinhaEntity> porId(@PathVariable Long id){
-		return ResponseEntity.ok(cozinhaRepository.porId(id));
+	public ResponseEntity<CozinhaEntity> porId(@PathVariable Long id) {
+		CozinhaEntity cozinha = cozinhaRepository.porId(id);
+
+		if (cozinha == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
 	}
 //	@GetMapping("/{id}")
 //	public CozinhaEntity porId(@PathVariable Long id){
 //		return cozinhaRepository.porId(id);
 //	}
-	
-	
+
 }
