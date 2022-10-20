2f50551cb57210b01d60933eaf0d50065777a011
Tzappiello <tzappiello@gmail.com>
2022-10-20 12:30:53 -0300

    Treino de spring implementando o método de deletar uma cozinha

--- src/main/java/com/zap/lojazap/api/controller/CozinhaController.java
+++ src/main/java/com/zap/lojazap/api/controller/CozinhaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -83,5 +84,12 @@ public class CozinhaController {
 //
 //		return ResponseEntity.ok(cozinhaAtual);
 //	}
+	
+	
+	@DeleteMapping("/{id}")
+	public void remover(@PathVariable Long  id) {
+		CozinhaEntity cozinha = cozinhaRepository.porId(id);
+		cozinhaRepository.remover(cozinha);
+	}
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,9 +2,9 @@ insert into cozinha (id, nome) values (1, 'Chinesa');
 insert into cozinha (id, nome) values (2, 'Mexicana');
 insert into cozinha (id, nome) values (3, 'Tailandesa');
 
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Mestre cuca', 14, 1) ;
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Du Chefe', 17, 2);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Gula', 17, 3);
+--insert into restaurante (nome, taxa_frete, cozinha_id) values ('Mestre cuca', 14, 1) ;
+--insert into restaurante (nome, taxa_frete, cozinha_id) values ('Du Chefe', 17, 2);
+--insert into restaurante (nome, taxa_frete, cozinha_id) values ('Gula', 17, 3);
 
 insert into forma_pagamento (descricao) values ('Dinheiro');
 insert into forma_pagamento (descricao) values ('Cartão');
