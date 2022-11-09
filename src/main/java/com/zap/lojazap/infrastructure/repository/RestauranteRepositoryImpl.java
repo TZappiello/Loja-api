9ae909d997988c76c4e400afad525a54fb3644c1
Tzappiello <tzappiello@gmail.com>
2022-11-09 15:10:18 -0300

    Treino de Spring Implementando criteria na pesquisa

--- src/main/java/com/zap/lojazap/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/zap/lojazap/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -27,7 +29,17 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
 		CriteriaQuery<RestauranteEntity> criteria = builder.createQuery(RestauranteEntity.class);
-		criteria.from(RestauranteEntity.class);
+		Root<RestauranteEntity> root = criteria.from(RestauranteEntity.class);
+
+		Predicate nomePredicate = builder.like(root.get("nome"), "%" +nome +"%");
+		
+		Predicate taxaInicial = builder.greaterThanOrEqualTo
+				(root.get("taxaFrete"), taxaFreteInicial);
+		
+		Predicate taxaFinal = builder.lessThanOrEqualTo
+				(root.get("taxaFrete"), taxaFreteFinal);
+		
+		criteria.where(nomePredicate, taxaInicial ,taxaFinal);
 		
 		TypedQuery<RestauranteEntity> query = manager.createQuery(criteria);
 		return query.getResultList();
