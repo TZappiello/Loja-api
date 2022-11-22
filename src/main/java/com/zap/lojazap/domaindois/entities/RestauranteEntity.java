7841e7da1bb99bfc2b1a5b719b19e935d4a7c1a4
Tzappiello <tzappiello@gmail.com>
2022-11-22 17:22:19 -0300

    Implementação da classe de endereço e feito a junção com a entidade Restaurante

--- src/main/java/com/zap/lojazap/domaindois/entities/RestauranteEntity.java
+++ src/main/java/com/zap/lojazap/domaindois/entities/RestauranteEntity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -44,6 +45,9 @@ public class RestauranteEntity {
 	@JoinColumn(name = "cozinha_id")
 	@ManyToOne
 	private CozinhaEntity cozinha;
+	
+	@Embedded
+	private Endereco endereco;
 	
 	@JsonIgnore
 	@ManyToMany
