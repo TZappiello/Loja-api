4585e8909f5fe96155186dfe52254df75cabc7e0
Tzappiello <tzappiello@gmail.com>
2022-12-12 16:37:37 -0300

    Aplicação de exceptions usando HttpStatus

--- src/main/java/com/zap/lojazap/domaindois/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/zap/lojazap/domaindois/exception/EntidadeNaoEncontradaException.java
@@ -1,5 +1,9 @@
 package com.zap.lojazap.domaindois.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EntidadeNaoEncontradaException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
