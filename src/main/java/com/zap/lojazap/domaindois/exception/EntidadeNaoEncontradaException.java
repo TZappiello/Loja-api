05cf55521a856ee10a0c307312f9c8c5ca0b4423
Tzappiello <tzappiello@gmail.com>
2022-12-20 11:17:50 -0300

    Melhorando as Exceptions

--- src/main/java/com/zap/lojazap/domaindois/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/zap/lojazap/domaindois/exception/EntidadeNaoEncontradaException.java
@@ -1,14 +1,19 @@
 package com.zap.lojazap.domaindois.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-public class EntidadeNaoEncontradaException extends RuntimeException {
+//@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EntidadeNaoEncontradaException extends ResponseStatusException {
 	private static final long serialVersionUID = 1L;
 	
+	
+	public EntidadeNaoEncontradaException(HttpStatus status, String mensagem) {
+		super(status, mensagem);
+	}
+
 	public EntidadeNaoEncontradaException(String mensagem) {
-		super(mensagem);
+		this(HttpStatus.NOT_FOUND, mensagem);
 	}
 
 }
--- src/main/java/com/zap/lojazap/domaindois/service/CadastroCozinhaService.java
+++ src/main/java/com/zap/lojazap/domaindois/service/CadastroCozinhaService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
