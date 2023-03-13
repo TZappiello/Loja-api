19edc599cc8d4b8050beaf75966da9b5a5cb0928
Tzappiello <tzappiello@gmail.com>
2023-03-13 12:47:14 -0300

    Tratamento de exception com erro no corpo da requisição

--- src/main/java/com/zap/lojazap/domaindois/exceptionhandler/ProblemType.java
+++ src/main/java/com/zap/lojazap/domaindois/exceptionhandler/ProblemType.java
@@ -5,9 +5,10 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade Não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
-	EXCEAO_DE_NEGOCIO("/entidade-nao-existente", "Entidade não existente");
+	ERRO_DE_NEGOCIO("/erro_negocio", "Violação de regra de negócio");
 	
 	
 	private String title;
