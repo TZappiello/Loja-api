95d349defb686652fb82edc7f30ad5c9be554601
Tzappiello <tzappiello@gmail.com>
2023-03-29 15:23:38 -0300

    Criação de um validação URL incorreta

--- src/main/java/com/zap/lojazap/domaindois/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/zap/lojazap/domaindois/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.zap.lojazap.domaindois.exceptionhandler;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
@@ -40,6 +42,32 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		Problem problem = CreateProblemBuilder(status, problemType, detail).build();
 
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request){
+		
+		if(ex instanceof MethodArgumentTypeMismatchException ) {
+			 return handleMethodArgumentTypeMismatch(
+		                (MethodArgumentTypeMismatchException) ex, headers, status, request);
+		 }
+		
+		return super.handleTypeMismatch(ex, headers, status, request);
+	}
+	
+	private ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+
+		ProblemType problemType = ProblemType.PARAMETRO_INVALIDO;
+		String detail = String
+				.format("O parâmetro de URL '%s' recebeu o valor '%s', que é de um tipo inválido."
+						+ " Corrija e informe um valor compatível com o tipo '%s'. ",ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName() );
+
+		Problem problem = CreateProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
 
 	private ResponseEntity<Object> handlePropertyBindingException(PropertyBindingException ex,
@@ -132,3 +160,4 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 				.detail(detail);
 	}
 }
+
