0e4e4239e044c40d87d4eb2528b25c4d794b790e
Tzappiello <tzappiello@gmail.com>
2023-03-30 13:58:59 -0300

    Aplicação de um validation na entidade cozinha.id

--- src/main/java/com/zap/lojazap/domaindois/entities/CozinhaEntity.java
+++ src/main/java/com/zap/lojazap/domaindois/entities/CozinhaEntity.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -27,6 +28,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class CozinhaEntity {
 
+	@NotNull
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
