c9af84525444ec93d3763d976ae1e8943619e704
Tzappiello <tzappiello@gmail.com>
2023-04-18 11:59:10 -0300

    Aplicação de um teste de classe com retorno 200 OK

--- src/test/java/com/zap/lojazap/CadastroCozinhaIT.java
+++ src/test/java/com/zap/lojazap/CadastroCozinhaIT.java
@@ -1,12 +1,33 @@
 package com.zap.lojazap;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-class LojaApiApplicationTests {
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CadastroCozinhaIT {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Test
+	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		RestAssured.given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
 
 /*	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
