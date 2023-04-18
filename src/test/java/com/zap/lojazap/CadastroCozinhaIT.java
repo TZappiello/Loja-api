420ab4f33560dcb0208e950fe8559437f26888e7
Tzappiello <tzappiello@gmail.com>
2023-04-18 15:42:44 -0300

    Criação de uma nova conexão de banco para teste

--- src/test/java/com/zap/lojazap/CadastroCozinhaIT.java
+++ src/test/java/com/zap/lojazap/CadastroCozinhaIT.java
@@ -8,12 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
-
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
 	
 	@LocalServerPort
