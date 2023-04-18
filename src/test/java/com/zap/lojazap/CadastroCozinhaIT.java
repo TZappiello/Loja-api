e459c75f8ed20948a77c59d74b16fcdb40a377f2
Tzappiello <tzappiello@gmail.com>
2023-04-18 12:26:25 -0300

    Criação de um novo teste de classe quando contem um nome

--- src/test/java/com/zap/lojazap/CadastroCozinhaIT.java
+++ src/test/java/com/zap/lojazap/CadastroCozinhaIT.java
@@ -1,5 +1,6 @@
 package com.zap.lojazap;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -28,6 +29,22 @@ class CadastroCozinhaIT {
 		.then()
 			.statusCode(HttpStatus.OK.value());
 	}
+	
+	@Test
+	public void deveConter3Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		RestAssured.given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", Matchers.hasSize(3))
+			.body("nome", Matchers.hasItem("Chinesa"));
+	}
+	
 
 /*	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
