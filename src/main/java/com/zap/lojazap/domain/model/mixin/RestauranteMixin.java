26ba02dc0e3549e551176349a79f301635608d92
Tzappiello <tzappiello@gmail.com>
2023-04-27 10:30:50 -0300

    Criação de uma classe mixin para uso de serialização

---
+++ src/main/java/com/zap/lojazap/domain/model/mixin/RestauranteMixin.java
@@ -0,0 +1,35 @@
+package com.zap.lojazap.domain.model.mixin;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.zap.lojazap.domaindois.entities.CozinhaEntity;
+import com.zap.lojazap.domaindois.entities.Endereco;
+import com.zap.lojazap.domaindois.entities.FormaPagamentoEntity;
+import com.zap.lojazap.domaindois.entities.ProdutoEntity;
+
+public abstract class RestauranteMixin   {
+
+
+	//@JsonIgnore // não vai mostra nenhuma cozinha
+	@JsonIgnoreProperties(value = "nome", allowGetters = true)
+	private CozinhaEntity cozinha;
+	
+	@JsonIgnore
+	private Endereco endereco;
+	
+	@JsonIgnore
+	private LocalDateTime dataCadastro;
+
+	@JsonIgnore
+	private LocalDateTime dataAtualizacao;
+	
+	@JsonIgnore
+	private List<FormaPagamentoEntity> formasPagamento = new ArrayList<>();
+	
+	@JsonIgnore // ignora a serialização da id.
+	private List<ProdutoEntity> produtos = new ArrayList<>();
+}
