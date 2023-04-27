8e7e3f20b827ef2cbb3d0298458400c8dbc36298
Tzappiello <tzappiello@gmail.com>
2023-04-27 10:45:16 -0300

    Criação de uma classe de mixin para cidade

--- src/main/java/com/zap/lojazap/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/zap/lojazap/core/jackson/JacksonMixinModule.java
@@ -3,7 +3,9 @@ package com.zap.lojazap.core.jackson;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.zap.lojazap.domain.model.mixin.CidadeMixin;
 import com.zap.lojazap.domain.model.mixin.RestauranteMixin;
+import com.zap.lojazap.domaindois.entities.CidadeEntity;
 import com.zap.lojazap.domaindois.entities.RestauranteEntity;
 
 @Component
@@ -13,5 +15,6 @@ public class JacksonMixinModule extends SimpleModule {
 
 	public JacksonMixinModule() {
 		setMixInAnnotation(RestauranteEntity.class, RestauranteMixin.class);
+		setMixInAnnotation(CidadeEntity.class, CidadeMixin.class);
 	}
 }
