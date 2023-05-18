2afe99c88fd259f17177fbecea95fe70466520ee
Tzappiello <tzappiello@gmail.com>
2023-05-18 12:15:18 -0300

    Criação do método de deletar uma forma de pagamento com todas as validaçõe

--- src/main/java/com/zap/lojazap/domaindois/service/CadastroFormaPagamentoService.java
+++ src/main/java/com/zap/lojazap/domaindois/service/CadastroFormaPagamentoService.java
@@ -1,17 +1,21 @@
 package com.zap.lojazap.domaindois.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.zap.lojazap.domaindois.entities.FormaPagamentoEntity;
+import com.zap.lojazap.domaindois.exception.CidadeNaoEncontradaException;
+import com.zap.lojazap.domaindois.exception.EntidadeEmUsoException;
 import com.zap.lojazap.domaindois.exception.FormaDePagamentoNaoEncontradoException;
 import com.zap.lojazap.domaindois.repository.FormaPagamentoRepository;
 
 @Service
 public class CadastroFormaPagamentoService {
 
-	private static final String MSG_CIDADE_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
+	private static final String MSG_CIDADE_EM_USO = "Forma de Pagamento de código %d não pode ser removida, pois está em uso";
 
 	@Autowired
 	private FormaPagamentoRepository formaPagamentoRepository;
@@ -22,20 +26,20 @@ public class CadastroFormaPagamentoService {
 
 	}
 
-//	@Transactional
-//	public void remover(Long id) {
-//		try {
-//			cidadeRepository.deleteById(id);
-//			cidadeRepository.flush();
-//			
-//		} catch (EmptyResultDataAccessException e) {
-//			throw new CidadeNaoEncontradaException(id);
-//
-//		} catch (DataIntegrityViolationException e) {
-//			throw new EntidadeEmUsoException(
-//					String.format(MSG_CIDADE_EM_USO, id));
-//		}
-//	}
+	@Transactional
+	public void remover(Long id) {
+		try {
+			formaPagamentoRepository.deleteById(id);
+			formaPagamentoRepository.flush();
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new FormaDePagamentoNaoEncontradoException(id);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(
+					String.format(MSG_CIDADE_EM_USO, id));
+		}
+	}
 
 	public FormaPagamentoEntity buscarSeTiver(Long id) {
 		return formaPagamentoRepository.findById(id).orElseThrow(() -> new FormaDePagamentoNaoEncontradoException(id));
