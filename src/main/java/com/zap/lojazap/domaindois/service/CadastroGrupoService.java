2a95b7bc083cb568817984c789dca07caf5cdcac
Tzappiello <tzappiello@gmail.com>
2023-05-22 15:18:52 -0300

    Criação do endPoint de delete na classe grupo

--- src/main/java/com/zap/lojazap/domaindois/service/CadastroGrupoService.java
+++ src/main/java/com/zap/lojazap/domaindois/service/CadastroGrupoService.java
@@ -1,32 +1,49 @@
 package com.zap.lojazap.domaindois.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zap.lojazap.domaindois.entities.GrupoEntity;
+import com.zap.lojazap.domaindois.exception.EntidadeEmUsoException;
 import com.zap.lojazap.domaindois.exception.GrupoNaoEncontradoException;
 import com.zap.lojazap.domaindois.repository.GrupoRepository;
 
 @Service
 public class CadastroGrupoService {
 
+	private static final String MSG_GRUPO_EM_USO = "Grupo de código %d não pode ser removida, pois está em uso";
+
 	@Autowired
 	private GrupoRepository grupoRepository;
-	
+
+	@Transactional
 	public GrupoEntity cadastrar(GrupoEntity grupoEntity) {
 		GrupoEntity entity = grupoRepository.save(grupoEntity);
-		
+
 		return entity;
 	}
-	
-	
+
 	public GrupoEntity buscarSeTiver(Long id) {
-		
-		GrupoEntity entity = grupoRepository.findById(id)
-				.orElseThrow(() -> new GrupoNaoEncontradoException(id));
-	
+
+		GrupoEntity entity = grupoRepository.findById(id).orElseThrow(() -> new GrupoNaoEncontradoException(id));
+
 		return entity;
+	}
+
+	@Transactional
+	public void excluir(Long id) {
+		try {
+			grupoRepository.deleteById(id);
+			grupoRepository.flush();
+
+		} catch (EmptyResultDataAccessException e) {
+			throw new GrupoNaoEncontradoException(id);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(String.format(MSG_GRUPO_EM_USO));
+		}
 	}
 }
-
-
