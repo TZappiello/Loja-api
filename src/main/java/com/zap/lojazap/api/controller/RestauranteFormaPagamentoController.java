7147b4fbc1d26f4e61c7f9f057613240e0228e7e
Tzappiello <tzappiello@gmail.com>
2023-05-23 17:31:08 -0300

    Criação dos endPoints de associar e desassociar uma forma de pagamento em restaurante

--- src/main/java/com/zap/lojazap/api/controller/RestauranteFormaPagamentoController.java
+++ src/main/java/com/zap/lojazap/api/controller/RestauranteFormaPagamentoController.java
@@ -3,9 +3,13 @@ package com.zap.lojazap.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zap.lojazap.api.DTO.FormaPagamentoDTO;
@@ -16,7 +20,6 @@ import com.zap.lojazap.domaindois.service.CadastroRestauranteService;
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/formas-pagamento")
 public class RestauranteFormaPagamentoController {
-
 
 	@Autowired
 	private CadastroRestauranteService cadastroRestaurante;
@@ -30,5 +33,16 @@ public class RestauranteFormaPagamentoController {
 		
 		return formaPagamentoModelAssembler.toCollectionDTO(restaurante.getFormasPagamento());
 	}
+	
+	@DeleteMapping("{formaPagamentoId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void desassociarFormaPagamento(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
+		cadastroRestaurante.desassociarFormaPagamento(restauranteId, formaPagamentoId);
+	}
 
+	@PutMapping("/{formaPagamentoId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void associarFormaPagamento(@PathVariable Long restauranteId, @PathVariable Long formaPagamentoId) {
+		cadastroRestaurante.associarFormaPagamento(restauranteId, formaPagamentoId);
+	}
 }
