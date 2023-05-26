bf72c582f9c0d583b145f15afd938656a6b076fc
Tzappiello <tzappiello@gmail.com>
2023-05-26 10:15:31 -0300

    Criação do endPoint de buscar um produto vinculado a um restaurante

---
+++ src/main/java/com/zap/lojazap/api/DTO/ProdutoDTO.java
@@ -0,0 +1,23 @@
+package com.zap.lojazap.api.DTO;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoDTO {
+	
+	private Long id;
+	
+	private String nome;
+	private String descricao;
+	private BigDecimal preco;
+	private boolean ativo;
+	
+//	private RestauranteDTO restaurante;
+	
+//	private List<ItemPedidoEntity> itemPedido = new ArrayList<>();
+
+}
