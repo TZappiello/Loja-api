91d4ee5d6cd335956389dfeec03776851c20e580
Tzappiello <tzappiello@gmail.com>
2023-06-05 17:17:27 -0300

    Refatoração do método de cadastrar um produto

--- src/main/java/com/zap/lojazap/api/assember/ProdutoModelInputAssembler.java
+++ src/main/java/com/zap/lojazap/api/assember/ProdutoModelInputAssembler.java
@@ -4,9 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.zap.lojazap.api.input.GrupoIdInput;
 import com.zap.lojazap.api.input.ProdutoInput;
-import com.zap.lojazap.domaindois.entities.GrupoEntity;
 import com.zap.lojazap.domaindois.entities.ProdutoEntity;
 
 @Component
@@ -16,13 +14,10 @@ public class ProdutoModelInputAssembler {
 	private ModelMapper modelMapper;
 
 	public ProdutoEntity toDTOObject(ProdutoInput produtoIdInput) {
-	
 		return modelMapper.map(produtoIdInput, ProdutoEntity.class);
 	}
 	
 	public void copyToDtoObject(ProdutoInput produtoInput, ProdutoEntity produtoEntity) {
-//		produtoInput.setId(produtoEntity.getId());
-
 		modelMapper.map(produtoInput, produtoEntity);
 	}
 }
--- src/main/java/com/zap/lojazap/api/controller/RestauranteProdutoController.java
+++ src/main/java/com/zap/lojazap/api/controller/RestauranteProdutoController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,14 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.zap.lojazap.api.DTO.EstadoDTO;
 import com.zap.lojazap.api.DTO.ProdutoDTO;
 import com.zap.lojazap.api.assember.ProdutoModelAssembler;
 import com.zap.lojazap.api.assember.ProdutoModelInputAssembler;
-import com.zap.lojazap.api.assember.RestauranteModelInputAssembler;
-import com.zap.lojazap.api.input.EstadoIdInput;
 import com.zap.lojazap.api.input.ProdutoInput;
-import com.zap.lojazap.domaindois.entities.EstadoEntity;
 import com.zap.lojazap.domaindois.entities.ProdutoEntity;
 import com.zap.lojazap.domaindois.entities.RestauranteEntity;
 import com.zap.lojazap.domaindois.repository.ProdutoRepository;
@@ -68,10 +63,10 @@ public class RestauranteProdutoController {
 	
 	@PostMapping()
 	@ResponseStatus(HttpStatus.CREATED)
-	public ProdutoDTO adicionar(@PathVariable Long restauranteId, @RequestBody @Valid ProdutoInput produtoIdInput) {
+	public ProdutoDTO adicionar(@PathVariable Long restauranteId, @RequestBody @Valid ProdutoInput produtoInput) {
 		RestauranteEntity restaurante = cadastroRestaurante.buscarSeTiver(restauranteId);
 		
-		ProdutoEntity produto = produtoModelInputAssembler.toDTOObject(produtoIdInput);
+		ProdutoEntity produto = produtoModelInputAssembler.toDTOObject(produtoInput);
 		produto.setRestaurante(restaurante);
 		
 		produto = cadastroProdutoService.cadastrar(produto);
@@ -81,23 +76,16 @@ public class RestauranteProdutoController {
 	}
 	
 	@PutMapping("/{produtoId}")
-	public ProdutoDTO atualizar(@PathVariable Long restauranteId, @PathVariable Long produtoId, @RequestBody @Valid ProdutoInput produtoIdInput) {
-		RestauranteEntity restaurante = cadastroRestaurante.buscarSeTiver(restauranteId);
+	public ProdutoDTO atualizar(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+			@RequestBody @Valid ProdutoInput produtoInput) {
 		
 		ProdutoEntity produto = cadastroProdutoService.buscarSeTiver(restauranteId, produtoId);
 		
-		produtoModelInputAssembler.copyToDtoObject(produtoIdInput, produto);
+		produtoModelInputAssembler.copyToDtoObject(produtoInput, produto);
 		
-//		ProdutoEntity produto1 = produtoModelInputAssembler.toDTOObject(produtoIdInput);
+		produto = cadastroProdutoService.cadastrar(produto);
 		
-		return produtoModelAssembler.toDTO(cadastroProdutoService.cadastrar(produto));
+		return produtoModelAssembler.toDTO(produto);
 	}
 	
 }
-		//@PutMapping("/{id}")
-		//public EstadoDTO atualizar(@PathVariable Long id, @RequestBody @Valid EstadoIdInput estadoIdInput) {
-		//	EstadoEntity estadoAtual = cadastroEstados.buscarSeTiver(id);
-		//
-		//	estadoModelInputAssembler.copyToDtoObject(estadoIdInput, estadoAtual);
-		//
-		//	return estadoModelAssembler.toDTO(cadastroEstados.adicionar(estadoAtual));
