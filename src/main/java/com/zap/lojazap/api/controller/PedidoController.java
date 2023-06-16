2387789d928ada3fb5492281b536da53379a2f0b
Tzappiello <tzappiello@gmail.com>
2023-06-16 18:54:48 -0300

    Criação do endPoint de buscar por id um pedido

--- src/main/java/com/zap/lojazap/api/controller/PedidoController.java
+++ src/main/java/com/zap/lojazap/api/controller/PedidoController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zap.lojazap.api.DTO.PedidoDTO;
 import com.zap.lojazap.api.assember.PedidoModelAssembler;
+import com.zap.lojazap.domaindois.entities.PedidoEntity;
 import com.zap.lojazap.domaindois.repository.PedidoRepository;
+import com.zap.lojazap.domaindois.service.CadastroPedidoService;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -21,13 +24,18 @@ public class PedidoController {
 	@Autowired
 	private PedidoModelAssembler pedidoModel;
 	
-//	@GetMapping
-//	public List<RestauranteDTO> listar() {
-//		return restauranteModelAssembler.toCollectionDTO(restauranteRepository.findAll());
-//	}
+	@Autowired
+	private CadastroPedidoService cadastroPedido;
 	
 	@GetMapping
 	public List<PedidoDTO> listar(){
 		return pedidoModel.toCollectionDTO(pedidoRepository.findAll());
+	}
+	
+	@GetMapping("{id}")
+	public PedidoDTO poId(@PathVariable Long id) {
+		PedidoEntity pedido = cadastroPedido.buscarSeTiver(id);
+		
+		return pedidoModel.toDTO(pedido);
 	}
 }
--- src/main/java/com/zap/lojazap/domaindois/repository/PedidoRepository.java
+++ src/main/java/com/zap/lojazap/domaindois/repository/PedidoRepository.java
@@ -1,11 +1,9 @@
 package com.zap.lojazap.domaindois.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.zap.lojazap.domaindois.entities.PedidoEntity;
 
-@Repository
 public interface PedidoRepository extends JpaRepository<PedidoEntity, Long> {
 
 }
