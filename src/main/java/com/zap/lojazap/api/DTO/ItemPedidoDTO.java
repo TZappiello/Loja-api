6573f49e4d770f5b20919e64560c39177fa62000
Tzappiello <tzappiello@gmail.com>
2023-06-16 14:35:02 -0300

    Criação do PedidoDTO, ItemDTO e RestauranteResumoDTO

---
+++ src/main/java/com/zap/lojazap/api/DTO/ItemPedidoDTO.java
@@ -0,0 +1,19 @@
+package com.zap.lojazap.api.DTO;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemPedidoDTO {
+
+	private Long produtoId;
+	private String nomeProduto;
+	private int quantidade;
+	private BigDecimal precoUnitario;
+	private BigDecimal precoTotal;
+	private String observacao;
+	
+}
