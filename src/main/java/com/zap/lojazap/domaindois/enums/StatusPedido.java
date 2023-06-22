865ae010bf47fa95e6484c4b4adc8ba4270abb73
Tzappiello <tzappiello@gmail.com>
2023-06-22 12:08:49 -0300

    Criação do método de fluxo do pedido com confirmação

--- src/main/java/com/zap/lojazap/domaindois/enums/StatusPedido.java
+++ src/main/java/com/zap/lojazap/domaindois/enums/StatusPedido.java
@@ -2,6 +2,20 @@ package com.zap.lojazap.domaindois.enums;
 
 public enum StatusPedido {
 	
-	CRIADO, CONFIRMADO, ENTREGUE, CANCELADO
+	CRIADO("Criado"), 
+	CONFIRMADO("Confirmado"),  
+	ENTREGUE("Entregue"), 
+	CANCELADO("Cancelado");
+
+
+	private String descricao;
+
+	private StatusPedido(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
 
 }
