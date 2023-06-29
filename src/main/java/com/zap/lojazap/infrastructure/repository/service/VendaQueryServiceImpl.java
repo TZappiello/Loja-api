5fa8ab195689794af621211543c494b7b6400155
Tzappiello <tzappiello@gmail.com>
2023-06-29 12:09:23 -0300

    Implementação por filtros e com status do pedido

--- src/main/java/com/zap/lojazap/infrastructure/repository/service/VendaQueryServiceImpl.java
+++ src/main/java/com/zap/lojazap/infrastructure/repository/service/VendaQueryServiceImpl.java
@@ -1,19 +1,65 @@
 package com.zap.lojazap.infrastructure.repository.service;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder.In;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
 
+import com.zap.lojazap.domaindois.entities.PedidoEntity;
 import com.zap.lojazap.domaindois.entities.model.VendaDiaria;
+import com.zap.lojazap.domaindois.enums.StatusPedido;
 import com.zap.lojazap.domaindois.filter.VendaDiariaFilter;
 import com.zap.lojazap.domaindois.service.VendaQueryService;
 
 @Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@Override
 	public List<VendaDiaria> consultaVendasDiarias(VendaDiariaFilter filtro) {
-		return null;
+		var builder = manager.getCriteriaBuilder(); // instanciando uma fabrica 
+		var query = builder.createQuery(VendaDiaria.class); //no query vai ser a classe de retorno não precisa ser a entidade
+		var root = query.from(PedidoEntity.class); // aqui faz o from da entidade
+		
+		var predicates = new ArrayList<Predicate>();
+		
+		var functionDateDataCriacao = builder.function("date", Date.class, root.get("dataCriacao"));
+		
+		var selection = builder.construct(VendaDiaria.class, 
+				functionDateDataCriacao,
+				builder.count(root.get("id")),
+				builder.sum(root.get("valorTotal")));
+		
+		
+		if(filtro.getRestauranteId() != null) {
+			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+		}
+		
+		if(filtro.getDataCriacao() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"),
+					filtro.getDataCriacao()));
+		}
+		
+		if(filtro.getDataFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), 
+					filtro.getDataFim()));
+		}
+		
+		predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE)); 
+		
+		query.select(selection);
+		query.where(builder.and(predicates.toArray(new Predicate[0])));
+		query.groupBy(functionDateDataCriacao);
+		
+		return manager.createQuery(query).getResultList();
 	}
 
 }
