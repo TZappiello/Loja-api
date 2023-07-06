c84acdf93c72360b18f488d8a442308b66e29e49
Tzappiello <tzappiello@gmail.com>
2023-07-06 15:55:11 -0300

    Criação de um método de salva imagem localmente

---
+++ src/main/java/com/zap/lojazap/infrastructure/repository/storege/StorageException.java
@@ -0,0 +1,16 @@
+package com.zap.lojazap.infrastructure.repository.storege;
+
+public class StorageException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	
+}
