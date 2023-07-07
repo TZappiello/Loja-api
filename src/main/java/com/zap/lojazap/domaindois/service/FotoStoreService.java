2bfd21005c01597db812ea55247b112bc6168179
Tzappiello <tzappiello@gmail.com>
2023-07-07 09:52:49 -0300

    Criação do método que salva a imagem no repositorio local

--- src/main/java/com/zap/lojazap/domaindois/service/FotoStoreService.java
+++ src/main/java/com/zap/lojazap/domaindois/service/FotoStoreService.java
@@ -1,13 +1,30 @@
 package com.zap.lojazap.domaindois.service;
 
 import java.io.InputStream;
+import java.util.UUID;
 
 import lombok.Builder;
 import lombok.Getter;
 
 public interface FotoStoreService {
 
+	InputStream recuperar(String nomeArquivo);
+	
 	void armazenar(NovaFoto novaFoto);
+	
+	void remover(String nomeArquivo);
+	
+	default void substituir(String nomeArquivoAntigo, NovaFoto novaFoto) {
+		this.armazenar(novaFoto);
+		
+		if(nomeArquivoAntigo != null) {
+			this.remover(nomeArquivoAntigo);
+		}
+	}
+
+	default String gerarNomeAquivo(String nomeOriginal) {
+		return  UUID.randomUUID()+"_"+ nomeOriginal;
+	}
 	
 	@Builder
 	@Getter
@@ -16,4 +33,5 @@ public interface FotoStoreService {
 		private String nomeAquivo;
 		private InputStream inputStream;
 	}
+
 }
