57402d348fdf16a26cb1b4f6b164eefa46397106
Tzappiello <tzappiello@gmail.com>
2023-07-07 17:29:48 -0300

    Ajuste properties

--- src/main/java/com/zap/lojazap/core/storage/StorageProperties.java
+++ src/main/java/com/zap/lojazap/core/storage/StorageProperties.java
@@ -5,6 +5,8 @@ import java.nio.file.Path;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.regions.Regions;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,7 +33,7 @@ public class StorageProperties {
 		private String idChaveAcesso;
 		private String chaveAcessoSecreta;
 		private String bucket;
-		private String regiao;
+		private Regions regiao;
 		private String diretorioFotos;
 	}
 }
