7d57bba0267f6d43f1ebc87ca5878ca0024646c2
Tzappiello <tzappiello@gmail.com>
2023-07-14 15:19:29 -0300

    Implementação do atributo dataAtualização e criação do magration e chamada Repositoy

--- src/main/java/com/zap/lojazap/domaindois/repository/FormaPagamentoRepository.java
+++ src/main/java/com/zap/lojazap/domaindois/repository/FormaPagamentoRepository.java
@@ -1,14 +1,18 @@
 package com.zap.lojazap.domaindois.repository;
 
+import java.time.OffsetDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.zap.lojazap.domaindois.entities.FormaPagamentoEntity;
 
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamentoEntity, Long> {
 
+	@Query("SELECT MAX(dataAtualizacao) FROM FormaPagamentoEntity")
+	OffsetDateTime getDataUltimaatualizacao();
 //	List<FormaPagamentoEntity> todas();
 //	FormaPagamentoEntity porId(Long id);
 //	FormaPagamentoEntity adicionar(FormaPagamentoEntity pagamento);
 //	void remover (FormaPagamentoEntity pagamento);
-//	
 }
