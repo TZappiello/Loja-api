a5d8a0e5efbf7a01deb1164eeed2c7216d83dbed
Tzappiello <tzappiello@gmail.com>
2023-07-18 11:32:00 -0300

    Ajuste na apresentação da documentação no swagger

--- src/main/java/com/zap/lojazap/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/zap/lojazap/core/openapi/SpringFoxConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -17,8 +20,18 @@ public class SpringFoxConfig implements WebMvcConfigurer {
         return new Docket(DocumentationType.OAS_30)
                 .select()
                   .apis(RequestHandlerSelectors.basePackage("com.zap.lojazap.api"))
-                  .build();
+                  .build()
+                 .apiInfo(apiInfo());
           }
+    
+    public ApiInfo apiInfo() {
+    	return new ApiInfoBuilder()
+    			.title("LojaZap API")
+    			.description("API aberta para clientes e restaurantes")
+    			.version("1")
+    			.contact(new Contact("Zap_API", "https://github.com/TZappiello", "contato@zappi.com.br"))
+    			.build();
+    }
     
 	// 	 http://localhost:8080/swagger-ui/#/       	 LINK PARA ACESSA O SWAGGER
     @Override  	// NÃO PRECISA USAR ESSE MÉTODO 	NEM DAS DEPENDENCIAS
