cb84e0e651d504bc5132cccc1ddf0e53b84b9c8d
Tzappiello <tzappiello@gmail.com>
2023-07-18 11:20:11 -0300

    Implementação do método para mostra o swagger no navegador

--- src/main/java/com/zap/lojazap/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/zap/lojazap/core/openapi/SpringFoxConfig.java
@@ -2,19 +2,28 @@ package com.zap.lojazap.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SpringFoxConfig {
+public class SpringFoxConfig implements WebMvcConfigurer {
 
     @Bean 
     Docket apiDockt() {		//DOCUMENTAÇÃO DO SWAGGER
         return new Docket(DocumentationType.OAS_30)
                 .select()
-                  .apis(RequestHandlerSelectors.any())
+                  .apis(RequestHandlerSelectors.basePackage("com.zap.lojazap.api"))
                   .build();
           }
+    
+	// 	 http://localhost:8080/swagger-ui/#/       	 LINK PARA ACESSA O SWAGGER
+    @Override  	// NÃO PRECISA USAR ESSE MÉTODO 	NEM DAS DEPENDENCIAS
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	registry.addResourceHandler("swagger-ui.html")
+    		.addResourceLocations("classpath:/META-INF/resources/");
+    }
 }
