11074f0d26a69f58d3080c7a7b8d5e07dc41234f
Tzappiello <tzappiello@gmail.com>
2023-08-09 15:13:46 -0300

    Implementação do token jwt na aplicação

--- src/main/java/com/zap/lojazap/core/security/ResourceServerConfig.java
+++ src/main/java/com/zap/lojazap/core/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.zap.lojazap.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -19,8 +24,15 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 					.anyRequest().authenticated()
 				.and()
 				.cors().and()
-				.oauth2ResourceServer().opaqueToken();
+				.oauth2ResourceServer().jwt();
 		
+	}
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+		var secretKey = new SecretKeySpec("uyrkfxzfuyflewyffuydfsyufluflugçiurehoihbcsjl".getBytes(), "HmacSHA256");
+    	
+    	return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 
 }
