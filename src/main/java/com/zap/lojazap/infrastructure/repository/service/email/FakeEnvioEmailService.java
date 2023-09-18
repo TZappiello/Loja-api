e8636c9f814b6be2c2e769a716abf0d7dd065eb3
Tzappiello <tzappiello@gmail.com>
2023-09-18 15:24:49 -0300

    Configuração de ambiente de dev e produção e ajuste de email

--- src/main/java/com/zap/lojazap/infrastructure/repository/service/email/FakeEnvioEmailService.java
+++ src/main/java/com/zap/lojazap/infrastructure/repository/service/email/FakeEnvioEmailService.java
@@ -1,15 +1,22 @@
 package com.zap.lojazap.infrastructure.repository.service.email;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zap.lojazap.domaindois.service.EnvioEmailService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class FakeEnvioEmailService extends SmtpEnvioEmailService {
+public class FakeEnvioEmailService implements EnvioEmailService {
+	
+	@Autowired
+	private ProcessadorEmailTemplate processadorEmailTemplate;
 
 	@Override
 	public void enviar(Mensagem mensagem) {
 		// Foi necessário alterar o modificador de acesso do método processarTemplate
 		// da classe pai para "protected", para poder chamar aqui
-		String corpo = processarTemplate(mensagem);
+		String corpo = processadorEmailTemplate.processarTemplate(mensagem);
 
 		log.info("[FAKE E-MAIL] Para: {}\n{}", mensagem.getDestinatarios(), corpo);
 	}
